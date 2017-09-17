2925cde93da98c71121a087910bb68ebfc15213a
Eric Roginek <eroginek@protonmail.com>
2017-09-17 19:55:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function lowWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  if (string.toLowerCase()) {
+    return "I can't hear you!"
+  }
+}
+
+function sayHiToGrandma(string){
+  if (string.toUpperCase()){
+    return "YES INDEED!"
+  }
+}
+
+function sayHiToGrandma(string){
+  if (string = "I love you, Grandma") {
+    return "I love you, too"
+  }
+}
