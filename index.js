4cb0aa9cc7b7e7983b132f3df3bfb92c077d970a
Eric Roginek <eroginek@protonmail.com>
2017-09-18 00:58:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string.toLowerCase()) {
-    return "I love you, too"
+  if (string = string.toLowerCase()) {
+    return "I can't hear you!"
   }
 }
 
