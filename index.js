40f11996a5ee6d91b8e427dd7b4ae99131251d03
Eric Roginek <eroginek@protonmail.com>
2017-09-18 02:27:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function sayHiToGrandma(string){
 function sayHiToGrandma(string){
   var lowercase = 'hello!'
   lowercase.toLowerCase() === lowercase
-  return "I love you, too."
+  return "I can't hear you!"
 }
 
 function sayHiToGrandma(string){
