8db2cbe26bddf0e068aba2d2e6b5eec3fbef6075
Eric Roginek <eroginek@protonmail.com>
2017-09-18 02:03:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string {
+function sayHiToGrandma(string){}
   var deaf = "I can't hear you!"
   return deaf
 }
