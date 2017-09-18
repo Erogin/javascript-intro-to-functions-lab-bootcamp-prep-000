8ed8d927ea372d9168a6314c969a5a4b7f7d2219
Eric Roginek <eroginek@protonmail.com>
2017-09-18 01:19:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string){
   var uppercase = "HELLO"
   var lowercase = 'hello'
   var love = "I love you, Grandma."
-  if (string.toEqual(uppercase)) {
+  if (string.toUpperCase === uppercase)) {
     return "YES INDEED!"
   }
   else if (string.toLowerCase === string) {
