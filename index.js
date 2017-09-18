a49fe4189a1c8dec0d77908e5dddd01dd1c98e80
Eric Roginek <eroginek@protonmail.com>
2017-09-18 01:25:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,29 +14,6 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string){
-  var uppercase = "HELLO"
-  var lowercase = 'hello'
-  var love = "I love you, Grandma."
-  if (string.toUpperCase) {
-    return "YES INDEED!"
-  }
-  else if (string.toLowerCase === string) {
-    return "I can't hear you!"
-  }
-  else {
-    return "I love you, too."
-  }
-}
-
-function sayHiToGrandma(string){
-  if (string.toUpperCase()){
-    return "YES INDEED!"
-  }
-}
-
-function sayHiToGrandma(string){
-  if (string = "I love you, Grandma") {
-    return "I love you, too"
-  }
+if (sayHiToGrandma("HELLO").toEqual(uppercase)){
+  return "I can't hear you!"
 }
